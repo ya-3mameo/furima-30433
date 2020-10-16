b385660a7233ff7a97d010e70cbbc27c3444af7f
ya-3mameo <nao51.gren@gmail.com>
2020-10-16 15:46:46 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,23 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :nickname, presence: true
+
+  validates :email, uniqueness: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+  validates :password, uniqueness: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+    validates :family_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ンーー]+\z/ } do
+    validates :family_name_furigana
+    validates :first_name_furigana
+  end
+  
+  validates :birthday, presence: true
 
 end
