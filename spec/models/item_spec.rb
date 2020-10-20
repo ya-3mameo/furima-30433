6af3d1f6124a345234e53f2406ae579ba5976552
ya-3mameo <nao51.gren@gmail.com>
2020-10-20 16:19:49 +0900

    テストコードの修正（id:1では保存できない）

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -30,27 +30,27 @@ RSpec.describe Item, type: :model do
       end
 
       it 'カテゴリーの情報が「---」のときは保存できない' do
-        @item.category_id = 0
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
       it '商品の状態についての情報が「---」のときは保存できない' do
-        @item.status_id = 0
+        @item.status_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Status can't be blank")
       end
       it '配送料の負担についての情報が「---」のときは保存できない' do
-        @item.burden_id = 0
+        @item.burden_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Burden can't be blank")
       end
       it '発送元の地域についての情報が「---」のときは保存できない' do
-        @item.prefecture_id = 0
+        @item.prefecture_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
       it '発送までの日数についての情報が「---」のときは保存できない' do
-        @item.delivery_day_id = 0
+        @item.delivery_day_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery day can't be blank")
       end
@@ -61,17 +61,17 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
       it '価格の範囲が、300円未満だと保存できない' do
-        @item.price = '100'
+        @item.price = 100
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
       it '価格の範囲が、9,999,999円だと保存できない' do
-        @item.price = '10000000'
+        @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include
       end
       it '販売価格は全角数字だと場合は保存できない' do
-        @item.price = '３４５'
+        @item.price = ３４５
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
