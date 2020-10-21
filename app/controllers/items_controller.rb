a472b39c2105a00f9e6ae0be6197f1ca12737af5
ya-3mameo <nao51.gren@gmail.com>
2020-10-21 16:58:44 +0900

    エラーメッセージを表示させる記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -30,9 +30,9 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    if item.update(item_params)
-      redirect_to item_path(item.id)
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
     else
       render :edit
     end
