8a98f8606cf6f9b7b4bae3efb6bfe41b28fa560b
ya-3mameo <nao51.gren@gmail.com>
2020-10-21 15:58:52 +0900

    ユーザーの状況によって表示を変える

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :move_to_session, except: [:index, :show]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -23,9 +25,39 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to item_path(item.id)
+    else
+      render :edit
+    end
+
+
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :status_id, :image, :burden_id, :prefecture_id, :delivery_day_id, :price).merge(user_id: current_user.id)
   end
+
+  def move_to_session
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    @user = User.find(@item.user_id)
+    unless user_signed_in? && current_user == @user
+      redirect_to root_path
+    end 
+  end
+
 end
