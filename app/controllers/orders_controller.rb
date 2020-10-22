a64110d54691382d3ec2d193a9d2c895ea4dbd4b
ya-3mameo <nao51.gren@gmail.com>
2020-10-22 18:18:22 +0900

    フォームの情報を保存する

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,22 @@
 class OrdersController < ApplicationController
+
   def index
+    @order_address = OrderAddress.new(order_params)
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def order_params
+    params.permit(:postal_number, :prefecture_id, :city, :house_number, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
 end
