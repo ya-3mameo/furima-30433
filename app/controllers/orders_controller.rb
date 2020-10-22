15df6e0ad35258f2b81d07b12a09a18132e7d4a9
ya-3mameo <nao51.gren@gmail.com>
2020-10-22 20:06:04 +0900

    更新失敗時に商品情報を保持する記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,12 @@
 class OrdersController < ApplicationController
 
   def index
-    @order_address = OrderAddress.new(order_params)
     @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       @order_address.save
