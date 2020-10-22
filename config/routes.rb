f9e424824fd0bb8ddb4a22a60c7f89d4b573b553
ya-3mameo <nao51.gren@gmail.com>
2020-10-22 12:08:03 +0900

    ルーティングのネスト設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "items#index"
-  resources :items
-  resources :orders
+  resources :items do
+    resources :orders, only: :index
+  end
 end
