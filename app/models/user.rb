5b50ae803a22b1217e611486d7ec299b79f27472
ya-3mameo <nao51.gren@gmail.com>
2020-10-22 12:32:11 +0900

    誤字修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
-  has_nany :orders
+  has_many :orders
 
   validates :email, uniqueness: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
