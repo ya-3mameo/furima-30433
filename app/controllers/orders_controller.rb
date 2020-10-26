9d8349ee7e2f2616e0d5cb046454848dcc999364
ya-3mameo <nao51.gren@gmail.com>
2020-10-26 15:03:55 +0900

    ストロングパラメーターの設置

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,23 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_order, only: [:index, :create]
+  before_action :move_to_root1, only: [:index, :create]
+  before_action :move_to_root2, only: [:index, :create]
+
 
   def index
-    @item = Item.find(params[:item_id])
-    @order_address = OrderAddress.new(order_params)
+    @order_address = OrderAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: Item.find(order_params[:item_id]).price,
+        card: params[:token],
+        currency: 'jpy'
+      )
       @order_address.save
       redirect_to root_path
     else
@@ -18,7 +27,18 @@ class OrdersController < ApplicationController
 
   private
   def order_params
-    params.permit(:postal_number, :prefecture_id, :city, :house_number, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    params.require(:order_address).permit(:order_address, :postal_number, :prefecture_id, :city, :house_number, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
   end
 
+  def move_to_root1
+    redirect_to root_path if user_signed_in? && current_user.id == @item.user_id
+  end
+
+  def move_to_root2
+    redirect_to root_path if @item.order.present?
+  end
+
+  def set_order
+    @item = Item.find(params[:item_id])
+  end
 end
