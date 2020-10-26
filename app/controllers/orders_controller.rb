7f9059830f80f378b86209bf5e2309f403bd6722
ya-3mameo <nao51.gren@gmail.com>
2020-10-26 16:39:03 +0900

    リファクタリング

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,12 +11,7 @@ class OrdersController < ApplicationController
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
-      Payjp.api_key = ENV['PAYJP_SECRET_KEY']
-      Payjp::Charge.create(
-        amount: Item.find(order_params[:item_id]).price,
-        card: params[:token],
-        currency: 'jpy'
-      )
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -28,6 +23,15 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_address).permit(:order_address, :postal_number, :prefecture_id, :city, :house_number, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: Item.find(order_params[:item_id]).price,
+      card: params[:token],
+      currency: 'jpy'
+    )
   end
 
   def move_to_root1
