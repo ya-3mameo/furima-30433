075b90e80d2911c252b97e55f3b367c7fa6efcdb
ya-3mameo <nao51.gren@gmail.com>
2020-10-26 15:06:26 +0900

    tokenのテストコード

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -67,6 +67,13 @@ RSpec.describe OrderAddress, type: :model do
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("Phone number is invalid")
       end
+
+      it 'tokenが空では保存できない' do
+        @order_address.token = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Token can't be blank")
+      end
+
     end
 
   end
