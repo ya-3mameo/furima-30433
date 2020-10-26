28cfac9f74d0bf4bf51942e8dfafccd2a54c410c
ya-3mameo <nao51.gren@gmail.com>
2020-10-26 15:05:17 +0900

    データ保存する値の修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :token, :user_id, :item_id, :postal_number, :prefecture_id, :city, :house_number, :building, :phone_number
+  attr_accessor :token, :order_id, :user_id, :item_id, :postal_number, :prefecture_id, :city, :house_number, :building, :phone_number
 
   with_options presence: true do
     validates :postal_number, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
@@ -8,15 +8,14 @@ class OrderAddress
     validates :city
     validates :house_number
     validates :phone_number, format: { with: /\A0[5789]0\d{4}\d{4}\z/ }
+    validates :token
   end
 
   validates :prefecture_id, numericality: { other_than: 1 }
 
   def save
-    user = User.create
-    item = Item.create
-    order = Order.create(user_id: user.id, item_id: item.id)
-    Address.creare(postal_number: postal_number, prefecture_id: prefecture_id, city: city, house_number: house_number,  building: building, phone_number: phone_number)
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_number: postal_number, prefecture_id: prefecture_id, city: city, house_number: house_number,  building: building, phone_number: phone_number, order_id: order_id)
   end
 
 
