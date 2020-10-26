f720ed1d4e4b90e594b131cac06c80c69aefc987
ya-3mameo <nao51.gren@gmail.com>
2020-10-26 16:43:16 +0900

    addressテーブルに保存するための修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :token, :user_id, :item_id, :postal_number, :prefecture_id, :city, :house_number, :building, :phone_number
+  attr_accessor :token, :order_id, :user_id, :item_id, :postal_number, :prefecture_id, :city, :house_number, :building, :phone_number
 
   with_options presence: true do
     validates :postal_number, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
@@ -14,7 +14,7 @@ class OrderAddress
   validates :prefecture_id, numericality: { other_than: 1 }
 
   def save
-    Order.create(user_id: user_id, item_id: item_id)
-    Address.create(postal_number: postal_number, prefecture_id: prefecture_id, city: city, house_number: house_number, building: building, phone_number: phone_number)
+    Order.create(user_id: user_id, item_id: item_id, order_id: order_id)
+    Address.create(postal_number: postal_number, prefecture_id: prefecture_id, city: city, house_number: house_number, building: building, phone_number: phone_number, order_id: order_id)
   end
 end
