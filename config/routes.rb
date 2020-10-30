42d1f24c0c5fa44f1a1354cf5d8324d62c5df942
ya-3mameo <nao51.gren@gmail.com>
2020-10-30 11:58:29 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   root to: "items#index"
   resources :items do
     resources :orders, only: [:index, :create]
+    collection do
+      get 'search'
+    end
   end
 end
